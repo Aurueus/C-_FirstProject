4a6f886e9846db146a9da70a29c0ebce4c8d6712
Davit Gevorkyan <gevorqiandavid05@gmail.com>
2025-10-01 20:19:58 +0400

    Finished sixth homework

---
+++ SixthHomework/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Test_Project
+{
+    public class Program
+    {
+        public static string LettersToBiggerRegister(string text)
+        {
+            char[] arr = text.ToCharArray();
+            bool isBigNext = true;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (isBigNext && Char.IsLetter(arr[i]))
+                {
+                    arr[i] = Char.ToUpper(arr[i]);
+                    isBigNext = false;
+                }
+
+                if (arr[i] == '.' || arr[i] == '!' || arr[i] == '?')
+                {
+                    isBigNext = true;
+                }
+            }
+
+            string result = new string(arr);
+            return result;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter text:");
+            string input = Console.ReadLine();
+            string result = LettersToBiggerRegister(input);
+            Console.WriteLine(result);
+        }
+    }
+}
